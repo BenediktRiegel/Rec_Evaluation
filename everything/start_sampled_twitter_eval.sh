064aba6edb92081bfec3f42b84295ac6fae088ab
Benedikt Riegel <Benedikt.Riegel@gmail.com>
2021-05-20 19:54:59 +0200

    I increased the runs for the sampled twitter dataset.

--- everything/start_sampled_twitter_eval.sh
+++ everything/start_sampled_twitter_eval.sh
@@ -6,5 +6,5 @@ kUFL_Path="${PWD}/sample/sample_kUFL_LocalSearch/"
 Rec_Path="${PWD}/sample/sample_Rec_LocalSearch/"
 
 ./sample/sample_LP/sample_LP_Algo ${Data_Path} 20 >> ${LP_Path}sample_twitter.out &
-./sample/sample_kUFL_LocalSearch/sample_kUFL_LocalSearch ${Data_Path} 1 >> ${kUFL_Path}sample_twitter.out &
-./sample/sample_Rec_LocalSearch/sample_RecLocalSearchEval ${Data_Path} 1 >> ${Rec_Path}sample_twitter.out &
+./sample/sample_kUFL_LocalSearch/sample_kUFL_LocalSearch ${Data_Path} 20 >> ${kUFL_Path}sample_twitter.out &
+./sample/sample_Rec_LocalSearch/sample_RecLocalSearchEval ${Data_Path} 20 >> ${Rec_Path}sample_twitter.out &
