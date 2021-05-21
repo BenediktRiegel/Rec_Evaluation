9a334117a31b4a5894ab3c8a6d4daeaae7fd53dc
Benedikt Riegel <Benedikt.Riegel@gmail.com>
2021-05-21 14:35:53 +0200

    changed name of dir "Source code" to "Source_code"

---
+++ everything/clean.sh
@@ -0,0 +1,14 @@
+#Normal
+make -C ./Source_code/kUFL_LocalSearch/ clean
+
+make -C ./Source_code/LP2/ clean
+
+make -C ./Source_code/Rec_LocalSearch/ clean
+
+
+#Sample
+make -C ./Source_code/sample_kUFL_LocalSearch/ clean
+
+make -C ./Source_code/sample_LP2/ clean
+
+make -C ./Source_code/sample_Rec_LocalSearch/ clean
