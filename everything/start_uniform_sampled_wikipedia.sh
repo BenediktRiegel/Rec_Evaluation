b8fd9f1a22b64714e6fd10785b08c67967ca09a5
Benedikt Riegel <Benedikt.Riegel@gmail.com>
2021-05-28 11:55:20 +0200

    Ordered the input parameters correctly for uniform sampling. Changed result directory for uniform sampling.

--- everything/start_uniform_sampled_wikipedia.sh
+++ everything/start_uniform_sampled_wikipedia.sh
@@ -6,5 +6,5 @@ kUFL_Path="${PWD}/uniform_sampling/uniform_sampling_kUFL_LocalSearch/"
 Rec_Path="${PWD}/uniform_sampling/uniform_sampling_Rec_LocalSearch/"
 
 ./uniform_sampling/uniform_sampling_LP/uniform_sampling_LP_Algo ${Data_Path} 1 >> ${LP_Path}uniform_sampled_wikipedia.out &
-./uniform_sampling/uniform_sampling_kUFL_LocalSearch/uniform_sampling_kUFL_LocalSearch 1 ${Data_Path} >> ${kUFL_Path}uniform_sampled_wikipedia.out &
-./uniform_sampling/uniform_sampling_Rec_LocalSearch/uniform_sampling_RecLocalSearchEval 1 ${Data_Path} >> ${Rec_Path}uniform_sampled_wikipedia.out &
+./uniform_sampling/uniform_sampling_kUFL_LocalSearch/uniform_sampling_kUFL_LocalSearch ${Data_Path} 1 >> ${kUFL_Path}uniform_sampled_wikipedia.out &
+./uniform_sampling/uniform_sampling_Rec_LocalSearch/uniform_sampling_RecLocalSearchEval ${Data_Path} 1 >> ${Rec_Path}uniform_sampled_wikipedia.out &
