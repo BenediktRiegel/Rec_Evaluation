2d6e7435a064d99cbafbc4bec0fe17a3b56595f8
Benedikt Riegel <Benedikt.Riegel@gmail.com>
2021-06-01 22:26:48 +0200

    Add TwitterDSampling to be an executable

--- everything/start_TwitterDSampling.sh
+++ everything/start_TwitterDSampling.sh
@@ -3,4 +3,5 @@
 Data_Path="${PWD}/Data/Twitter/"
 TwitterDSampling_Path="${PWD}/TwitterDSampling/"
 
+chmod +x ./TwitterDSampling/TwitterDSampling
 ./TwitterDSampling/TwitterDSampling ${Data_Path} ${TwitterDSampling_Path} >> ${TwitterDSampling_Path}output.out
