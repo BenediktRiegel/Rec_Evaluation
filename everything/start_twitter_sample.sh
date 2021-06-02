7201ef49a00f22410bbf09342590bc5ffc8145cc
Benedikt Riegel <Benedikt.Riegel@gmail.com>
2021-06-02 18:55:55 +0200

    Twitter_sample: Removed some comments. Added version.

--- everything/start_twitter_sample.sh
+++ everything/start_twitter_sample.sh
@@ -5,5 +5,5 @@ Data_Path="${PWD}/Data/Twitter/"
 kUFL_Path="${PWD}/Twitter_sample/Twitter_sample_kUFL_LocalSearch/"
 Rec_Path="${PWD}/Twitter_sample/Twitter_sample_Rec_LocalSearch/"
 
-./Twitter_sample/Twitter_sample_kUFL_LocalSearch/Twitter_sample_kUFL_LocalSearch ${Data_Path} 1 >> ${kUFL_Path}sample_twitter.out &
-./Twitter_sample/Twitter_sample_Rec_LocalSearch/Twitter_sample_RecLocalSearchEval ${Data_Path} 1 >> ${Rec_Path}sample_twitter.out &
+./Twitter_sample/Twitter_sample_kUFL_LocalSearch/Twitter_sample_kUFL_LocalSearch ${Data_Path} 10 0 500 >> ${kUFL_Path}sample_twitter.out &
+./Twitter_sample/Twitter_sample_Rec_LocalSearch/Twitter_sample_RecLocalSearchEval ${Data_Path} 10 0 500 >> ${Rec_Path}sample_twitter.out &
