b6007ce8b7c2da13a9f816d46129014bb78d22a3
Benedikt Riegel <Benedikt.Riegel@gmail.com>
2021-06-03 11:41:12 +0200

    Added quotations for start_congress_naiveRec.sh

--- everything/start_congress_naiveRec.sh
+++ everything/start_congress_naiveRec.sh
@@ -2,8 +2,8 @@
 
 Data_Path="${PWD}/Data/Congress/"
 naiveRec_Path="${PWD}/naiveRec/"
-comparekUFL_Path="/EvaluationResults/kUFLLocalSearch/compareNaiveRec/
-compareLP_Path="/EvaluationResults/LP/compareNaiveRec/
+comparekUFL_Path="/EvaluationResults/kUFLLocalSearch/compareNaiveRec/"
+compareLP_Path="/EvaluationResults/LP/compareNaiveRec/"
 
 ./naiveRec/naive_RecLocalSearchEval ${Data_Path} ${comparekUFL_Path} 10 16 >> ${naiveRec_Path}LP_output.out &
 ./naiveRec/naive_RecLocalSearchEval ${Data_Path} ${compareLP_Path} 10 16 >> ${naiveRec_Path}kUFL_output.out &
